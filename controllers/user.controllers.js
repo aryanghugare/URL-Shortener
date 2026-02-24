d9ba6995520327e32fe462a6cf1b9e2e1fe59151
Aryan <aryanghugare75@gmail.com>
2026-02-24 22:22:27 +0530

    Implement user authentication with login functionality and JWT token generation

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,9 +1,12 @@
 import { db } from "../db/index.js";
 import { usersTable } from "../models/user.model.js";
-import { signupPostRequestBodySchema } from "../validation/request.validation.js";
+import { signupPostRequestBodySchema , loginPostRequestBodySchema } from "../validation/request.validation.js";
 import {hashPasswordWithSalt} from "../utils/hash.js";
 import { eq } from "drizzle-orm";
 import { getUserByEmail } from "../services/user.service.js";
+import jwt from 'jsonwebtoken';
+import { createUserToken } from "../utils/token.js";
+
 
 
 
@@ -41,3 +44,39 @@ res.status(201).json({ message: "User created successfully", user });
 }
 
 }
+
+export const login = async (req, res) => {
+try {
+const validationResult = await loginPostRequestBodySchema.safeParseAsync(req.body);
+
+if(validationResult.error){
+    return res.status(400).json({ error: validationResult.error.message });
+}
+
+const { email, password } = validationResult.data;
+const user = await getUserByEmail(email);
+
+if(!user){
+    return res.status(400).json({ error: "User with this email does not exist" });
+}
+const { password: hashedPassword } = hashPasswordWithSalt(password, user.salt); // This hashed password should be same as that of the password stored in the database
+
+if (hashedPassword !== user.password) {
+    return res.status(400).json({ error: "Invalid password" });
+}
+
+const token = await createUserToken({ id: user.id });
+res.status(200).json({ message: "Login successful", token });
+
+} catch (error) {
+    console.error("Error during login:", error);
+    res.status(401).json({ error: "Error while logging in" });
+}
+
+
+
+
+
+
+}
+
